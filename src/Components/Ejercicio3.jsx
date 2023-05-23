0b5e28563d972ea7f26c7c98c02ed80c56649c7d
Aixa Filsinger <macafil99@gmail.com>
2023-05-23 11:56:00 -0300

    se empezo el ejercicio , se limpio el proyecto

---
+++ src/Components/Ejercicio3.jsx
@@ -0,0 +1,13 @@
+
+
+const ejercicio3 = () => {
+    return (
+        <section>
+            <h1 className="display-1 text-center">Hello my Friend</h1>
+        </section>
+            
+        
+    );
+};
+
+export default ejercicio3;
