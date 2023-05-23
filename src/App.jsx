5d0c53553d3b4f386d10cb00e258d5f3479fa81a
Aixa Filsinger <macafil99@gmail.com>
2023-05-23 14:32:30 -0300

    se termino el ejercicio3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
 import './App.css'
 import Ejercicio3 from './Components/Ejercicio3'
+import {Container}   from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
 
   return (
+    
     <>
-      <Ejercicio3></Ejercicio3>
+    <Container className="my-5 main"><Ejercicio3></Ejercicio3></Container>
+      
+
+    
+       <footer className="bg-dark text-light text-center py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
   )
 }
--- src/Components/Ejercicio3.jsx
+++ src/Components/Ejercicio3.jsx
@@ -1,10 +1,21 @@
 
-
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 const ejercicio3 = () => {
+    const [click, setClick] = useState('');
+    
+
+   
     return (
-        <section>
-            <h1 className="display-1 text-center">Hello my Friend</h1>
-        </section>
+        <>
+         <h1 className="display-2 text-center fw-semibold text-light">Hello my Friend{click}</h1>
+         <aside className='text-center mt-5'>
+            <Button variant="light" size="lg" onClick={()=> setClick(' (from changed state)!')}>Click</Button>
+         </aside>
+            
+        </>
+           
+        
             
         
     );
